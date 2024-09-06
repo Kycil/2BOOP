999a106de16b470e7e0ae5290ff0daaee63ec6da
Your Name <you@example.com>
2024-09-06 15:40:43 +0800

    first commit

---
+++ first.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Name: Kycil Matin-ao <br>";
+    echo "Address: Centro Hulpa Tudela Misamis Occidental <br>";
+    echo "Contact Number: 09753045791 <br>";
+    echo "Parents: Ponciana Liquin and Reynard Matin-ao <br>";
+    echo "Social Media: Kycil Matin-ao <br>";
+
+    echo "<h1>Assignment nako</h1> <br>";
+    ?>
